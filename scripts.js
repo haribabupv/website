b20d800eaab14d3bd6bfa2e4bbdfe7d1a46e80fb
Hariprasanth Palanivel <haribabupv354@gmail.com>
2024-08-03 18:39:15 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// This script can be used for interactive features
+document.addEventListener('DOMContentLoaded', function() {
+    // Example of an interactive feature
+    console.log('Page is fully loaded and DOM is ready');
+});
+
